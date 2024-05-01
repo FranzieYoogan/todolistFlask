323ea756229ad17104f24cd3ba06ba73e976ed76
FrankieSilvaSantos <frankiesilvasantos@gmail.com>
2024-05-01 12:20:35 -0300

    logicStuff

--- todo/.venv/Scripts/app.py
+++ todo/.venv/Scripts/app.py
@@ -8,6 +8,7 @@ from datetime import date
 
 @app.route("/",methods=['POST','GET'])
 def home():
+  
  
  
   mydb = mysql.connector.connect(
@@ -24,33 +25,58 @@ def home():
   
   print(result)
 
+  
+ 
+  
+
+
+
   if(request.method == "POST"):
 
 
+     
 
 
+      inputValue = request.form['inputValue']
+      today = date.today()
+      inputValue = request.form['inputValue']
+      today = date.today()
+      if(inputValue):
+        cursor.execute(f"insert into todoTask (todoTask,todoDate) values('{inputValue}','{today}')")
+        mydb.commit()
+        return render_template('home.htm', data = result)
+      else:
+        error = True
+        return render_template('home.htm',error = error,data = result)
+    
 
-    inputValue = request.form['inputValue']
-    today = date.today()
-    inputValue = request.form['inputValue']
-    today = date.today()
-    if(inputValue):
-      cursor.execute(f"insert into todoTask (todoTask,todoDate) values('{inputValue}','{today}')")
-      mydb.commit()
-      return render_template('home.htm', data = result)
 
   return render_template('home.htm', data = result)
 
 @app.route("/tasks",methods=['POST','GET'])
-def task():
- 
- 
-
- 
+def tasks():
   
+ 
+  mydb = mysql.connector.connect(
+    host="127.0.0.1",
+    user="root",
+    password="",
+    database="todo_schema"
+    )
   
+  cursor = mydb.cursor()
+  cursor.execute("select * from todoTask")
+  result = cursor.fetchall()
 
-  return render_template('tasks.htm')
+  if(request.method == "POST"):
+    checkBox = request.form['checkBox']
+    cursor.execute(f"DELETE FROM todoTask WHERE todoTask = '{checkBox}'")
+    mydb.commit()
+    cursor.execute("select * from todoTask")
+    result = cursor.fetchall()
+
+    return render_template('tasks.htm',data = result)
+  return render_template('tasks.htm',data = result)
 
 
 app.run(debug=True) 
